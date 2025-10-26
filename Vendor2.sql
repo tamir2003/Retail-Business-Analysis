4d235f1da89eb62011f11ccb1744aafbd5f123fd
Tamir Qureshi <142157799+tamir2003@users.noreply.github.com>
2025-10-26 20:51:37 +0530

    Update Vendor2.sql

--- Vendor2.sql
+++ Vendor2.sql
@@ -1,22 +1,50 @@
-Select trim(VendorName) from Vendor_sale_summary
+--------------------------------------------
+--          BASIC VENDOR QUERIES
+--------------------------------------------
 
-Select VendorName,(TotalSalesDollars-TotalPurchaseDollars) as GrossProfit from Vendor_sale_summary
-
-Select VendorName,((TotalSalesDollars-TotalPurchaseDollars)/TotalSalesDollars)*100 as ProfitMargin from Vendor_sale_summary
-
-Select VendorName,TotalSalesQuantity/TotalPurchaseQuantity as StockTurnOver from Vendor_Sale_Summary
-
-Select VendorName, TotalSalesDollars/TotalPurchaseDollars as SalesToPurchase_Ratio From Vendor_sale_summary
-
-Select * ,
-(TotalSalesDollars-TotalPurchaseDollars) as GrossProfit ,
-((TotalSalesDollars-TotalPurchaseDollars)/TotalSalesDollars)*100 as ProfitMargin,
-(TotalSalesQuantity/TotalPurchaseQuantity) as StockTurnOver,
-(TotalSalesDollars/TotalPurchaseDollars) as SalesToPurchase_Ratio 
-into VendorSales_summary
-from Vendor_sale_summary
+-- 1. Trim Vendor Names
+SELECT 
+    TRIM(VendorName) AS VendorName
+FROM Vendor_sale_summary;
 
 
+-- 2. Gross Profit Calculation
+SELECT 
+    VendorName,
+    (TotalSalesDollars - TotalPurchaseDollars) AS GrossProfit
+FROM Vendor_sale_summary;
 
 
+-- 3. Profit Margin (%)
+SELECT 
+    VendorName,
+    ((TotalSalesDollars - TotalPurchaseDollars) / TotalSalesDollars) * 100 AS ProfitMargin
+FROM Vendor_sale_summary;
 
+
+-- 4. Stock Turnover Ratio
+SELECT 
+    VendorName,
+    (TotalSalesQuantity / TotalPurchaseQuantity) AS StockTurnOver
+FROM Vendor_sale_summary;
+
+
+-- 5. Sales to Purchase Ratio
+SELECT 
+    VendorName,
+    (TotalSalesDollars / TotalPurchaseDollars) AS SalesToPurchase_Ratio
+FROM Vendor_sale_summary;
+
+
+--------------------------------------------
+--     CREATE ENRICHED VENDOR SALES SUMMARY
+--------------------------------------------
+
+SELECT 
+    *,
+    (TotalSalesDollars - TotalPurchaseDollars) AS GrossProfit,
+    ((TotalSalesDollars - TotalPurchaseDollars) / TotalSalesDollars) * 100 AS ProfitMargin,
+    (TotalSalesQuantity / TotalPurchaseQuantity) AS StockTurnOver,
+    (TotalSalesDollars / TotalPurchaseDollars) AS SalesToPurchase_Ratio
+INTO VendorSales_summary
+FROM Vendor_sale_summary;
